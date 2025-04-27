187affc82ca6ed378fc0e4ba5be679ba5f390d7a
jiwon-mun <jw.mun@birdview.kr>
2025-04-27 12:21:47 +0900

    cors

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import reviewsRouter from "./routes/reviews";
 import { initDB } from "./db/init";
+import cors from "cors";
 
 dotenv.config();
 
 (async () => {
   await initDB(); // 테이블 먼저 준비
 
+  // CORS 미들웨어 사용 (모든 도메인 허용)
+
   const app = express();
   const port = process.env.PORT || 3000;
+  app.use(cors());
 
   app.use(express.json());
 
